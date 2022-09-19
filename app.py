aa2595804970f617b776b9660ec4b6577c99e364
tenoob <18bcs6218@gmail.com>
2022-09-19 23:51:55 +0530

    done with exception package

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from housing.logger import logging
+from housing.exception import HousingException
+import sys
+
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    logging.info("logging started")
+    try:
+        raise Exception("testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)   
+        logging.info(housing.error_message)
+        logging.info("Testing custom exception")
     return "Hello"
 
 
