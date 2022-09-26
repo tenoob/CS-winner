47f27891b5f4065d1abf0feff5a03ebca4de0798
tenoob <18bcs6218@gmail.com>
2022-09-27 00:20:05 +0530

    rectified the logging problem

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -19,7 +19,7 @@ class HousingException(Exception):
         
         _,_,exec_tb = error_detail.exc_info()
         
-        line_number = exec_tb.tb_frame.f_lineno
+        line_number = exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
 
         error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
